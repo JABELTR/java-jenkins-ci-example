0aafb703b7c03e8cc92629e3f1f764316b479845
Jairo Arturo Beltrán Suárez <jairo.beltrans@gmail.com>
2025-03-20 14:54:47 -0500

    Ajustar hola mundo otra vez

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -2,7 +2,7 @@ package com.ejemplo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("¡Hola, hola Mundo!");
+        System.out.println("¡Hola, hola este es el Mundo!");
     }
 }
 
