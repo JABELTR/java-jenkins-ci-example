4541d4761c3fca6b1512a50152364f2a5a15138a
Jairo Arturo Beltrán Suárez <jairo.beltrans@gmail.com>
2025-03-24 10:40:02 -0500

    Ajustar mensaje Hola mundo

--- src/main/java/com/ejemplo/App.java
+++ src/main/java/com/ejemplo/App.java
@@ -2,7 +2,7 @@ package com.ejemplo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("¡Hola, hola este es el Mundo!");
+        System.out.println("¡Hola, este es el Mundo!");
     }
 }
 
